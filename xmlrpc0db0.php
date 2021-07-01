f0c257e6f74283bf80f2608954d21e2a655dcb9e
Blessing Mashoko <bmashcom@hotmail.com>
2021-07-01 13:37:38 +0200

    initial upload

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://clevers.github.io</homePageLink>
+		<homePageLink>https://cleversresorts.github.io</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://clevers.github.io/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://clevers.github.io/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://clevers.github.io/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://clevers.github.io/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://clevers.github.io/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://cleversresorts.github.io/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://cleversresorts.github.io/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://cleversresorts.github.io/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://cleversresorts.github.io/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://cleversresorts.github.io/wp-json/" />
 			</apis>
 	</service>
 </rsd>
